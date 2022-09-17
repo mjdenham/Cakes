91b4af7e80ea11f51837e07aa1ac2bd37d04af2f
MartinDenham <martin.denham@faithlife.com>
2022-09-17 21:16:21 +0100

    Show network error message and allow retry

--- app/src/main/java/com/martin/cakes/CakesViewModel.kt
+++ app/src/main/java/com/martin/cakes/CakesViewModel.kt
@@ -10,6 +10,7 @@ import com.martin.cakes.model.CakesClient
 import com.martin.cakes.model.ICakesClient
 import com.martin.cakes.ui.theme.CakesResponse
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
@@ -18,6 +19,11 @@ class CakesViewModel(private val cakesClient: ICakesClient = CakesClient(), priv
     //TODO migrate to MutableStateFlow eventually
     private val cakes = MutableLiveData<CakesResponse>()
     private val refreshing = MutableLiveData<Boolean>(false)
+
+    val coroutineExceptionHandler = CoroutineExceptionHandler{_, throwable ->
+        Log.e(TAG, "Error loading cakes", throwable)
+        cakes.postValue(CakesResponse.Error)
+    }
 
     init {
         loadCakes()
@@ -29,9 +35,13 @@ class CakesViewModel(private val cakesClient: ICakesClient = CakesClient(), priv
 
     fun isRefreshing(): LiveData<Boolean> = refreshing
 
+    fun refresh() {
+        loadCakes()
+    }
+
     private fun loadCakes() {
         Log.d(TAG, "Loading cakes")
-        viewModelScope.launch(dispatcher) {
+        viewModelScope.launch(dispatcher + coroutineExceptionHandler) {
             cakes.postValue(CakesResponse.Loading)
             refreshing.postValue(true)
             val response = cakesClient.getCakes()
@@ -47,10 +57,6 @@ class CakesViewModel(private val cakesClient: ICakesClient = CakesClient(), priv
             }
             refreshing.postValue(false)
         }
-    }
-
-    fun refresh() {
-        loadCakes()
     }
 
     companion object {
