733d5339a6e9d9bec00467c7c81c65c464875d97
MartinDenham <martin.denham@faithlife.com>
2022-09-17 14:07:13 +0100

    sealed response to prepare for error response

--- app/src/main/java/com/martin/cakes/CakesViewModel.kt
+++ app/src/main/java/com/martin/cakes/CakesViewModel.kt
@@ -5,22 +5,22 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.martin.cakes.model.CakeDto
 import com.martin.cakes.model.CakesClient
 import com.martin.cakes.model.ICakesClient
+import com.martin.cakes.ui.theme.CakesResponse
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class CakesViewModel(private val cakesClient: ICakesClient = CakesClient()): ViewModel() {
 
     //TODO migrate to MutableStateFlow eventually
-    private val cakes: MutableLiveData<List<CakeDto>> by lazy {
-        MutableLiveData<List<CakeDto>>().also {
+    private val cakes: MutableLiveData<CakesResponse> by lazy {
+        MutableLiveData<CakesResponse>().also {
             loadCakes()
         }
     }
 
-    fun getCakes(): LiveData<List<CakeDto>> {
+    fun getCakes(): LiveData<CakesResponse> {
         return cakes
     }
 
@@ -28,9 +28,10 @@ class CakesViewModel(private val cakesClient: ICakesClient = CakesClient()): Vie
         viewModelScope.launch(Dispatchers.IO) {
             val response = cakesClient.getCakes()
             if (response.isSuccessful) {
-                cakes.postValue(response.body())
+                cakes.postValue(CakesResponse.Success(response.body() ?: emptyList()))
             } else {
                 Log.e(TAG, "Error querying repo ${response.message()}")
+                cakes.postValue(CakesResponse.Error)
             }
         }
     }
